e05832621f0df7915dc799df9a95f7e7785bb80b
AlekseyShlapakov <aash271@gmail.com>
2020-07-26 20:52:02 +0300

    refactoring code

--- src/index.js
+++ src/index.js
@@ -13,13 +13,14 @@ const gameEngine = (gameTask, generateParams) => {
     const [question, correctAnswer] = generateParams();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (correctAnswer !== userAnswer) {
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
-    console.log('Correct!');
-    i += 1;
   }
   console.log(`Congratulations, ${userName}!`);
 };
