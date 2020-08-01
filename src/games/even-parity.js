9023199f632f616c8c53fabe43c6ab2bbe3a6a9f
AlekseyShlapakov <aash271@gmail.com>
2020-08-01 23:18:35 +0300

    refactoring code

--- src/games/even-parity.js
+++ src/games/even-parity.js
@@ -1,16 +1,13 @@
 import runGameEngine from '../index.js';
 import generateRandomNum from '../utils.js';
 
-const isNotEven = (number) => {
-  if (number % 2 === 0) return false;
-  return true;
-};
+const isEven = (number) => number % 2 === 0;
 
 const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const generateParams = () => {
   const question = generateRandomNum(1, 21);
-  const correctAnswer = isNotEven(question) ? 'no' : 'yes';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
